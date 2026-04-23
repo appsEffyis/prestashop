48874407ba419bcea0a375b20d84b0e7a66eb51f
appsEffyis <apps@lodinpay.com>
2026-04-23 12:51:16 +0100

    version tester + prod

---
+++ module/lodin/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Lodin RTP Payment Module
+ * Generates payment links via Effyis API
+ *
+ * @author    Lodin < apps@lodinpay.com>
+ * @copyright 2026 Lodin
+ * @license Lodin Proprietary License
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
