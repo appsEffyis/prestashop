2828a1339702fe6d4fe2b364df5f51a84d47a8d3
appsEffyis <apps@lodinpay.com>
2026-04-29 10:06:11 +0100

    test n2

--- module/lodin/lodin.php
+++ module/lodin/lodin.php
@@ -7,10 +7,12 @@
  * @copyright 2026 Lodin
  * @license Lodin Proprietary License
  */
-use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
+
+use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
 
 class Lodin extends PaymentModule
 {
